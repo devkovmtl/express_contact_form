07b4965f717a837c40ba3a13286f0a0f98ad824f
Mikael <dev.kov.mtl@gmail.com>
2022-05-14 10:47:00 -0400

    routes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const path = require('path');
 const express = require('express');
 
+const { sendContactEmail } = require('./mailer');
+
 const { PORT } = process.env;
 
 const app = express();
@@ -9,6 +11,34 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+/**
+ * @description Accept contact form data and send it to the server
+ *
+ * @api POST /api/contact
+ *
+ * @data {string} name, {string} email, {string} message
+ *
+ * @access public
+ */
+app.post('/api/contact', async (req, res) => {
+  const { name, email, message } = req.body;
+  try {
+    await sendContactEmail({ to: email, name, message });
+
+    return res.status(200).json({
+      success: true,
+      message: 'Email sent successfully',
+      data: { name, email, message },
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: 'Unable to process request',
+      data: {},
+    });
+  }
+});
 
 app.get('/', (req, res) => {
   res.json({ success: true, data: 'Hello World' });
